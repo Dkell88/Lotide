29c4914c59c863f8c0c9fbb4bb86830e6a91782b
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-04 18:04:03 -0700

    Changed the console log to use template strings

--- assertEqual.js
+++ assertEqual.js
@@ -4,8 +4,8 @@ const num = 88;
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
-  } else console.log("Assertion Failed: " + actual + " !== " + expected);
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else console.log(`Assertion Failed: ${actual} !== ${expected}`);
 };
 
 
