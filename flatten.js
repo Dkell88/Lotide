18d2ebb157c2c52e6dab395dcd166f046a5983e5
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-06 15:39:29 -0700

    Removed all instances of else where not needed

--- flatten.js
+++ flatten.js
@@ -15,7 +15,9 @@ const assertArraysEqual = function(arr1, arr2) {
   
   if (eqArrays(arr1, arr2) === true) {
     console.log(`Assertion Passed: ${arr1.join()} === ${arr2.join()}`);
-  } else console.log(`Assertion Failed: ${arr1.join()} !== ${arr2.join()}`);
+    return;
+  } 
+  console.log(`Assertion Failed: ${arr1.join()} !== ${arr2.join()}`);
 
 };
 
--- middle.js
+++ middle.js
@@ -26,7 +26,9 @@ const assertArraysEqual = function(arr1, arr2) {
 
   if (eqArrays(arr1, arr2)) {
     console.log(`Assertion Passed: ${arr1.join()} === ${arr2.join()}`);
-  } else console.log(`Assertion Failed: ${arr1.join()} !== ${arr2.join()}`);
+    return;
+  } 
+  console.log(`Assertion Failed: ${arr1.join()} !== ${arr2.join()}`);
 
 };
 
